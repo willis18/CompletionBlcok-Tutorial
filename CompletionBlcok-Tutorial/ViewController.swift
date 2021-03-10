fad45eeeeaae49d514cd01c5ee6bec9b98a05d90
willis18 <willis18@naver.com>
2021-03-11 08:12:15 +0900

    컴플레이션 블럭 / 비동기처리

--- CompletionBlcok-Tutorial/ViewController.swift
+++ CompletionBlcok-Tutorial/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import KRProgressHUD
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mainTitle: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("VieController - viewDidLoad() 호출됨")
+        KRProgressHUD.show()
+        sayHi(completion: {result in
+            print(result)
+            KRProgressHUD.showSuccess()
+            self.mainTitle.text = result
+            
+        })
     }
 
-
+    fileprivate func sayHi(completion: @escaping(String) -> ()){
+        print("ViewController - sayHi()호출됨")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) { // Change `2.0` to the desired number of seconds.
+           // Code you want to be delayed
+            print("하이")
+            completion("하이끝")
+        }
+    }
 }
 
